6661fa98929696b6329242fd974ce0a50f33c16c
Suprawee Sowanna <suprawee.so@ku.th>
2025-08-21 16:21:46 +0700

    Modified StudentTest.java test other methods

--- src/test/java/ku/cs/models/StudentTest.java
+++ src/test/java/ku/cs/models/StudentTest.java
@@ -8,10 +8,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
+
+    @Test
+    void testConstructorWithNoScore(){
+        Student s = new Student("6710405222","Suprawee Sowanna");
+
+        assertEquals("6710405222", s.getId());
+        assertEquals("Suprawee Sowanna", s.getName());
+        assertEquals(0, s.getScore());
+    }
+
+    @Test
+    void testConstructorWithScore(){
+        Student s = new Student("6710405222","Suprawee Sowanna", 67);
+
+        assertEquals("6710405222", s.getId());
+        assertEquals("Suprawee Sowanna", s.getName());
+        assertEquals(67, s.getScore());
+    }
+
+
+
     Student s;
     @BeforeEach
     void init(){
         s=new Student("6710405222","Suprawee Sowanna");
+    }
+
+    @Test
+    void testChangeName(){
+        s.changeName("Sup Sow");
+        assertEquals("Sup Sow", s.getName());
     }
 
     @Test
@@ -27,6 +54,15 @@ class StudentTest {
         assertEquals("F", s.grade());
     }
 
+    @Test
+    void testIsId(){
+        assertEquals(true,s.isId("6710405222"));
+    }
+
+    @Test
+    void testIsNameContains(){
+        assertEquals(true, s.isNameContains("suprawee sowanna"));
+    }
 
 
 }
